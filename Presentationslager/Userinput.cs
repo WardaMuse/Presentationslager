a312b0e2d8b01cf007cb3a076c308b9eefeb0255
WardaMuse <157599911+WardaMuse@users.noreply.github.com>
2024-02-05 12:52:21 +0100

    Userinput - Warda

---
+++ Presentationslager/Userinput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Presentationslager
+{
+    public static class Userinput
+    {
+        public static int UserInt(string text)
+        {
+            Console.WriteLine(text);
+            int.TryParse(Console.ReadLine(), out int input);
+            return input;
+
+        }
+
+        public static string UserString(string text)
+        {
+            Console.WriteLine(text);
+            return Console.ReadLine();
+        }
+    }
+}
